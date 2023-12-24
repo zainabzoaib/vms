fb7e30b9e39a03aa26ff50d122328bf0d97e2875
Zainab Murtaza <zainabmurtaza2517@gmail.com>
2023-12-24 18:26:51 +1100

    Login page added

--- src/Pages/RegisterEoi.js
+++ src/Pages/RegisterEoi.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 function RegisterEoi() {
-    return ( <div><p> Hello world!</p></div>
+    return ( 
+        <section>
+            <div className='w-screen h-screen'>
+                <div className='w-full h-full App App-link'><p>Page 1</p></div>
+                <div className='w-full h-full'><p>Page 2</p></div>
+                </div>
+        </section>
     )
 }
 export default RegisterEoi
