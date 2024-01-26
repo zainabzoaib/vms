83185ea10b9c419e126baa24f07b85e390c673b2
Zainab Murtaza <zainabmurtaza2517@gmail.com>
2024-01-26 16:05:46 +1100

    Fixing pie chart responsiveness

--- src/Pages/components/PieChartsRow.jsx
+++ src/Pages/components/PieChartsRow.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { PieChart, Pie, Cell, Legend, Tooltip, Label } from "recharts";
+import { PieChart, Pie, Cell, Legend, Tooltip, Label,ResponsiveContainer } from "recharts";
 import Typography from "@mui/material/Typography";
 import axios from "axios";
 import { styled } from "@mui/material/styles";
@@ -16,19 +16,22 @@ const PieChartWithText = (props) => {
         className="flex shadow-lg bg-white rounded-md p-4 border-1 border-red w-full"
         style={{ textAlign: "left" }}
       >
-        <div>
-          <PieChart width={130} height={100}>
+        <div style={{ width: '100%', height: '150px'}}>
+      <ResponsiveContainer width="100%" height="100%">
+          <PieChart>
             <Pie
               data={data}
               dataKey={datakey}
-              cx="50%"
-              cy="50%"
-              innerRadius={30}
-              outerRadius={50}
+              innerRadius={45}
+              outerRadius="100%"
               startAngle={810}
               // endAngle={0}
               fill={color}
-              sx={{ width: '100%' }}
+              sx={{
+                cx:"50%",
+                cy:"50%",
+              }
+              }
             >
               <Cell key={`cell-0`} fill={COLORS[0]} />
               <Cell key={`cell-1`} fill={COLORS[1]} />
@@ -36,13 +39,15 @@ const PieChartWithText = (props) => {
             {/* <Legend verticalAlign="bottom" /> */}
             {/* <Tooltip /> */}
           </PieChart>
-        </div>
-        <div className="pt-5">
+          </ResponsiveContainer>
+          </div>
+        
+        <div className="pt-5 items-center flex">
           <Typography variant="p" gutterBottom>
             {title} : {total}
           </Typography>
         </div>
-      </div>
+        </div>
     </section>
   );
 };
